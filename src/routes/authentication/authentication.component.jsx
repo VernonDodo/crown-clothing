8387e13570986d77c5abb989ae77d6e4c8e7c386
Vernon <vcdodo@gmail.com>
2022-07-21 21:03:43 -0400

    Completed authentication component

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -7,10 +7,12 @@ import {
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
+import '../authentication/authentication.styles.scss';
+
 const Authentication = () => {
     
     return (
-        <div>
+        <div className='authentication-container'>
             <SignInForm />
             <SignUpForm />
             </div>
