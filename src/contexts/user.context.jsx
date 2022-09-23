46bf707e1f3241e86489b99e8020634461c1684a
Vernon <vcdodo@gmail.com>
2022-09-22 21:26:24 -0400

    Changed the context files to use reducer function

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,10 @@
 import { createContext, useEffect, useReducer } from "react";
 
+import { createAction } from '../utils/reducer/reducer.utils';
+
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
+
+
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -20,11 +24,11 @@ const userReducer = (state, action) => {
     const { type, payload } = action;
 
     switch(type) {
-        case 'SET_CURRENT_USER':
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: payload
-            }
+            };
         default:
             throw new Error(`Unhandled type ${type} in userReducer`)
 
@@ -38,7 +42,7 @@ export const UserProvider = ({ children }) => {
     const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE)
 
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
     }
 
     const value = { currentUser, setCurrentUser };
@@ -58,3 +62,12 @@ export const UserProvider = ({ children }) => {
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
 
+/*
+
+const userReducer = (state, action) => {
+    return {
+        currentUser:
+    }
+}
+
+*/
